52423a326b9188cba39306d948a3b9dfda09bcc9
Tanaka Yasufumi <yasufumi.tanaka75@gmail.com>
2021-11-20 14:50:47 +0900

    コントローラ修正、マイグレーションファイル修正

--- TECH.I.S_laravel/database/migrations/2021_10_23_115835_create_learning_plans_table.php
+++ TECH.I.S_laravel/database/migrations/2021_10_23_115835_create_learning_plans_table.php
@@ -14,7 +14,7 @@ class CreateLearningPlansTable extends Migration
     public function up()
     {
 
-        Schema::create('learning-plans', function (Blueprint $table) {
+        Schema::create('learning_plans', function (Blueprint $table) {
 
             $table->integer('id')->autoIncrement();
             $table->integer('user_id')->index();
